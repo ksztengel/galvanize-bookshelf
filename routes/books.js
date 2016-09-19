c0ba908e6f0e759d459415d10a6ee70e74296dbb
Kristin Sztengel <kcsztengel@gmail.com>
2016-09-19 16:09:13 -0600

    first draft server error content type not /json/

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,96 @@
 'use strict';
 
 const express = require('express');
-
-// eslint-disable-next-line new-cap
 const router = express.Router();
-
+const knex = require('../knex');
 // YOUR CODE HERE
+router.get('/books', (_req, res, next) => {
+    knex('books')
+        .orderBy('id')
+        .then((books) => {
+            res.set('Content-Type', 'application/json');
+            res.send(books);
+        })
+        .catch((err) => {
+            next(err);
+        });
+});
+
+router.get('/books/:id', (req, res, next) => {
+    knex('books')
+        .where('id', req.params.id)
+        .first()
+        .then((book) => {
+            if (!Books) {
+                return next();
+            }
+            res.set('Content-Type', 'application/json');
+            res.send(book);
+        })
+        .catch((err) => {
+            next(err);
+        });
+});
+router.post('/books', (req, res, next) => {
+    knex('books')
+        .insert({
+            name: req.body.name
+        }, '*')
+        .then((books) => {
+            res.set('Content-Type', 'application/json');
+            res.send(books[0]);
+        })
+        .catch((err) => {
+            next(err);
+        });
+});
+router.patch('/books/:id', (req, res, next) => {
+    knex('books')
+        .where('id', req.params.id)
+        .first()
+        .then((book) => {
+            if (!book) {
+                return next();
+            }
+
+            return knex('books')
+                .update({
+                    name: req.body.name
+                }, '*')
+                .where('id', req.params.id);
+        })
+        .then((books) => {
+            res.set('Content-Type', 'application/json');
+            res.send(books[0]);
+        })
+        .catch((err) => {
+            next(err);
+        });
+});
+// router.delete('/books/:id', (req, res, next) => {
+//   let artist;
+//
+//   knex('books')
+//     .where('id', req.params.id)
+//     .first()
+//     .then((row) => {
+//       if (!row) {
+//         return next();
+//       }
+//
+//       book = row;
+//
+//       return knex('book')
+//         .del()
+//         .where('id', req.params.id);
+//     })
+//     .then(() => {
+//       delete book.id;
+//       res.send(book);
+//     });
+//     .catch((err) => {
+//       next(err);
+// });
+// });
 
 module.exports = router;
