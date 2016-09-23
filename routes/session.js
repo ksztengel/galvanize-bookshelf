90f6c1ff7be6eb2e137ff0045b9650bedb198105
Kristin Sztengel <kcsztengel@gmail.com>
2016-09-23 10:21:55 -0600

    sessions complete

--- routes/session.js
+++ routes/session.js
@@ -5,43 +5,47 @@ const router = express.Router();
 const knex = require('../knex');
 const humps = require('humps');
 const bcrypt = require('bcrypt');
+const boom = require('boom');
 
-router.get('/', function (req,res,next){
-  if (req.session.userInfo){
-    res.send(true)
-  }
-  else {
-    res.send(false)
-  }
-})
-
-router.post ('/', function (req,res, next){
-  knex('users')
-  .where('email', req.body.email)
-  .then(function(results){
-    if (!results){
-      res.send('Bad email or password')
-    } else
-
-    {
-      var user = results[0]
-      var passwordMatch = bcrypt.compareSync(req.body.password, user.hashed_password)
-      if(passwordMatch == false){
-        res.send('Username or email does not match')
-    } else
-    {
-      delete user.hashed_password
-        req.session.userInfo = user
-        res.send(humps.camelizeKeys(user))
-
-      }
+router.get('/', function(req, res, next) {
+    if (req.session.userInfo) {
+        res.send(true)
+    } else {
+        res.send(false)
     }
-  })
 })
 
-router.delete('/', function(req,res,next) {
-  req.session = null
-  res.send(true)
+router.post('/', function(req, res, next) {
+    knex('users')
+        .where('email', req.body.email)
+        .first()
+        .then(function(results) {
+            if (!results) {
+                throw boom.create(400, 'Bad email or password')
+            } else
+
+            {
+                var user = results
+                var passwordMatch = bcrypt.compareSync(req.body.password, user.hashed_password)
+                console.log(passwordMatch);
+                if (passwordMatch == false) {
+                    throw boom.create(400, 'Bad email or password')
+                } else {
+                    delete user.hashed_password
+                    req.session.userInfo = user
+                    res.send(humps.camelizeKeys(user))
+
+                }
+            }
+        })
+        .catch((err) => {
+            next(err);
+        });
+})
+
+router.delete('/', function(req, res, next) {
+    req.session = null
+    res.send(true)
 })
 
 module.exports = router;
