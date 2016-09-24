468b010b02d83e9d7ea3f0ec6975cee4ef83916d
Kristin Sztengel <kcsztengel@gmail.com>
2016-09-24 10:23:06 -0600

    POST favorites working

--- routes/favorites.js
+++ routes/favorites.js
@@ -7,11 +7,11 @@ const humps = require('humps');
 const bcrypt = require('bcrypt');
 const boom = require('boom');
 
-const authorize = function(req, res, next){
-  if (!req.session.userInfo){
-    throw boom.create(401,'Unauthorized')
-  }
-  next()
+const authorize = function(req, res, next) {
+    if (!req.session.userInfo) {
+        throw boom.create(401, 'Unauthorized')
+    }
+    next()
 }
 
 router.get('/', authorize, (req, res, next) => {
@@ -37,13 +37,33 @@ router.get('/:id', authorize, (req, res, next) => {
                 res.send(true);
             } else {
 
-              res.send(false);
+               res.send(false);
             }
         })
         .catch((err) => {
             next(err)
         })
 });
+
+router.post('/', authorize, (req, res, next) => {
+    const insertFav = {
+        book_id: req.body.bookId,
+        user_id: req.session.userInfo.id
+    };
+
+    knex('favorites')
+        .insert(humps.decamelizeKeys(insertFav), '*')
+
+    .then((books) => {
+            const favorite = humps.camelizeKeys(books[0])
+            console.log(books[0]);
+            res.send(favorite)
+        })
+        .catch((err) => {
+            next(err)
+        })
+})
+
 
 
 module.exports = router;
